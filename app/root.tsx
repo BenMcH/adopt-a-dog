817347569767383451d3859085c52c65b72a6fc9
Ben McHone <benjamin.mchone@gmail.com>
2022-01-01 20:01:04 -0600

    Body padding

--- app/root.tsx
+++ app/root.tsx
@@ -22,7 +22,7 @@ export default function App() {
         <Links />
         <link rel="stylesheet" href="/tailwind.css" />
       </head>
-      <body>
+      <body className="p-5">
         <Outlet />
         <h2 className="text-xl">This site currently pulls from the <a className="underline underline-offset-2" href="https://www.arl-iowa.org/">ARL of Iowa</a> and <a className="underline underline-offset-2" href="https://www.aheinz57.com/">AHeinz57</a> rescue.</h2>
         <p className="text-xs">Note: this site has no affiliation with any animal shelters used as sources. The goal of this site is simply to help puppers and doggos find loving forever homes</p>
